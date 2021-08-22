4985ea75f1593fc451a83c8122c1aba7b367b045
cm296 <cat.magri@gmail.com>
2021-08-22 16:47:45 -0400

    Initial commit

---
+++ .ipynb_checkpoints/run_depth_script-checkpoint.py
@@ -0,0 +1,7 @@
+
+import run_depth 
+model_name = 'mono+stereo_640x192'
+output_directory = '../depthEstimate'
+no_cuda = True
+object2vec = False
+run_depth.test_simple(model_name,output_directory,no_cuda, object2vec)
